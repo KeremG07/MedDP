80c50bd6b5bbe1a926782d568e09236c20d3aa43
KeremG07 <69321438+KeremG07@users.noreply.github.com>
2023-01-07 21:48:38 +0300

    gui functionality added

--- src/main.py
+++ src/main.py
@@ -6,11 +6,23 @@ else:
     # Python 3
     import tkinter as tk
 
+query_list = ["Query 1", "Query 2", "Query 3"]
+
 root = tk.Tk()
 root.resizable(False, False)
 root.geometry('318x480')
 root.title("MedDP")
 
+variable = tk.StringVar(root)
+variable.set(query_list[0]) # default value
 
+query_dropdown = tk.OptionMenu(root, variable, *query_list)
+query_dropdown.pack()
+
+button = tk.Button(root, text="OK", command=lambda: query_result_label.config(text=variable.get()))
+button.pack()
+
+query_result_label = tk.Label(root)
+query_result_label.pack()
 
 root.mainloop()
